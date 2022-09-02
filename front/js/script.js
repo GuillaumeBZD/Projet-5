fed495ca33ea873c41db095cd900e0e3c9551ef8
Golf_Bravo <102588344+GuillaumeBZD@users.noreply.github.com>
2022-09-02 15:46:59 +0200

    Projet 5 V0.1

--- front/js/script.js
+++ front/js/script.js
@@ -1,37 +1,56 @@
 // récup des valeurs produits
-const lesCanaps = fetch("http://127.0.0.1:5500/back/models/Product.js")
+const lesCanaps = fetch("http://localhost:3000/api/products/")
   .then(function (res) {
     if (res.ok) {
-      return response.json();
+      return res.json();
     }
   })
   .then(function (value) {
     console.log(value);
+    affichageProduit(value);
   })
   .catch(function (err) {
     //une erreur est survenue
   });
 
-const produits = res.json();
-
 //affichage des élements produit
-const kanap01 = produits[0];
+function affichageProduit(resultat) {
+  if (resultat.length > 0) {
+    const kanap01 = resultat[0];
 
-const imageProduit = document.createElement("img");
-imageProduit.src = kanap01.imageUrl;
+    const lienProduit = genererProduit(
+      kanap01._id,
+      kanap01.imageUrl,
+      kanap01.name,
+      kanap01.description
+    );
 
-const nomProduit = document.createElement("h3");
-nomProduit.innerText = kanap01.name;
+    //récupération de l'élément du DOM qui acceuillera les fiches
+    const emplacementProduit = document.getElementById("items");
 
-const descriptionProduit = document.createElement("p");
-descriptionProduit.innerText = kanap01.description ?? "aucune description";
+    //Ajout des elements produit crees dans le DOM
+    emplacementProduit.appendChild(lienProduit);
+  }
+}
 
-//récupération de l'élément du DOM qui acceuillera les fiches
+function genererProduit(urlProduit, urlImage, name, description) {
+  const baliseA = document.createElement("a");
+  baliseA.setAttribute("href", "./product.html?id=" + urlProduit);
+  const articleProduit = document.createElement("article");
+  const imageProduit = document.createElement("img");
+  imageProduit.src = urlImage;
 
-const emplacementProduit = document.getElementById("items");
+  const nomProduit = document.createElement("h3");
+  nomProduit.innerText = name;
 
-//Ajout des elements produit crees dans le DOM
+  const descriptionProduit = document.createElement("p");
+  descriptionProduit.innerText = description ?? "aucune description";
 
-emplacementProduit.appendChild(imageProduit);
-emplacementProduit.appendChild(nomProduit);
-emplacementProduit.appendChild(descriptionProduit);
+  baliseA.appendChild(articleProduit);
+  articleProduit.appendChild(imageProduit);
+  articleProduit.appendChild(nomProduit);
+  articleProduit.appendChild(descriptionProduit);
+  return baliseA;
+}
+
+// generation des fiche produit avec une boucle for
